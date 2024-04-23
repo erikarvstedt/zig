ee07600763484475b6fa0a9da8832628d14e9ed5
Erik Arvstedt <erik.arvstedt@gmail.com>
2024-04-23 22:13:53 +0200

    build_runner: use unicode chars instead of DEC escape sequences

--- lib/compiler/build_runner.zig
+++ lib/compiler/build_runner.zig
@@ -770,7 +770,7 @@ fn printPrefix(node: *PrintNode, stderr: File, ttyconf: std.io.tty.Config) !void
     } else {
         try stderr.writeAll(switch (ttyconf) {
             .no_color, .windows_api => "|  ",
-            .escape_codes => "\x1B\x28\x30\x78\x1B\x28\x42  ", // │
+            .escape_codes => "│  ",
         });
     }
 }
@@ -778,7 +778,7 @@ fn printPrefix(node: *PrintNode, stderr: File, ttyconf: std.io.tty.Config) !void
 fn printChildNodePrefix(stderr: File, ttyconf: std.io.tty.Config) !void {
     try stderr.writeAll(switch (ttyconf) {
         .no_color, .windows_api => "+- ",
-        .escape_codes => "\x1B\x28\x30\x6d\x71\x1B\x28\x42 ", // └─
+        .escape_codes => "└─ ",
     });
 }
 
@@ -941,7 +941,7 @@ fn printTreeStep(
         } else {
             try stderr.writeAll(switch (ttyconf) {
                 .no_color, .windows_api => "+- ",
-                .escape_codes => "\x1B\x28\x30\x74\x71\x1B\x28\x42 ", // ├─
+                .escape_codes => "├─ ",
             });
         }
     }
